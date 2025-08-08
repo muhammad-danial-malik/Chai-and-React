fae1846874393e3e68a4c0a2c20cbbe756e78022
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-08-08 22:33:19 +0500

    Study Project

--- 01-lecture/src/App.jsx
+++ 01-lecture/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -10,6 +11,9 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
