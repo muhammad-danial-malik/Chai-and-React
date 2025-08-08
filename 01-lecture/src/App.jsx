079bdb8ccbdbe0dd02cebcfd9a65dce62ee1b52b
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-08-08 22:32:11 +0500

    Study Project

--- 01-lecture/src/App.jsx
+++ 01-lecture/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -12,11 +11,9 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Study Project</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
