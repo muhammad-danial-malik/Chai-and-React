6c02f08c0a652aceae6df06a28728f9125d25be9
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-08-09 21:58:01 +0500

    Add Test component to demonstrate code structure

---
+++ lecture-02/src/components/Test.jsx
@@ -0,0 +1,10 @@
+const Test = () => {
+  return (
+    <div>
+      <h1>Test Component</h1>
+      <p>This is a test component to demonstrate code structure.</p>
+    </div>
+  );
+};
+
+export default Test;
